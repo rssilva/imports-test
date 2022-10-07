5972f592803bc3a02efa93868a4fc7aeead92e66
Rafael Specht <rafaelspdasilva@gmail.com>
2022-10-06 23:35:53 -0300

    definitely some imports here

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Input } from '../components'
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>this is home</h1>
+      <Button>hola!</Button>
+      <Input />
+    </div>
+  )
+}
