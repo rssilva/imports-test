a647a9748a752d64f5c67e4b0aeb6371ccb4d8dd
Rafael Specht <rafaelspdasilva@gmail.com>
2022-10-09 22:50:42 -0300

    removing files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css'
 import { Home } from './pages/home'
+import { Button } from './components'
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <Home />
     </div>
   )
